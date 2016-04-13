70a36db11ddaef01abe37f985bbb1842373b451d
vm <vladappleseed@gmail.com>
2016-04-12 22:36:16 -0600

    Current commit (up to date as of 04/12/16 @ 10:34 pm)

---
+++ desktop/src/com/mygdx/gravswarm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.gravswarm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.gravswarm.GravSwarm;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GravSwarm(), config);
+	}
+}
