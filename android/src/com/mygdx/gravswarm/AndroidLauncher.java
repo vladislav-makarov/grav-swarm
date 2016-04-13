31b12e8f8cd6ded41f46d12e580a0237ba0a4b42
vm <vladappleseed@gmail.com>
2016-04-12 22:27:46 -0600

    test

---
+++ android/src/com/mygdx/gravswarm/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.gravswarm;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.gravswarm.GravSwarm;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GravSwarm(), config);
+	}
+}
