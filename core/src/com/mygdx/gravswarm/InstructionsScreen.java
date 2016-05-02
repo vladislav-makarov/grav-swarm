4d8618d41156b76f5460869e63882b9808978e95
vm <vladappleseed@gmail.com>
2016-05-01 23:35:53 -0600

    - settings screen is now functional and populated with settings options , and option choices for those settings options respectively. - designed & added on-screen back button to Settings and Instructions screens - designed graphics elements for the settings screen - other minor changes

--- core/src/com/mygdx/gravswarm/InstructionsScreen.java
+++ core/src/com/mygdx/gravswarm/InstructionsScreen.java
@@ -3,11 +3,9 @@ package com.mygdx.gravswarm;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
